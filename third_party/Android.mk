3108c690d8fa83bc0d9522dd1e3d668f896286af
Brandon McAnsh <brandon.mcansh@gmail.com>
2016-09-17 12:41:41 -0400

    Add optional build guard for bigint
    * Some devices may need this still
    
    Change-Id: I0a36c05c52faa29a3cfb358101314df618cf088c
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- third_party/Android.mk
+++ third_party/Android.mk
@@ -1,7 +1,9 @@
 BASE_PATH := $(call my-dir)
 
 include $(BASE_PATH)/pdfiumagg23.mk
+ifeq ($(TARGET_NEEDS_PDFIUM_BIGINT),true)
 include $(BASE_PATH)/pdfiumbigint.mk
+endif
 include $(BASE_PATH)/pdfiumlcms.mk
 include $(BASE_PATH)/pdfiumjpeg.mk
 include $(BASE_PATH)/pdfiumopenjpeg.mk
